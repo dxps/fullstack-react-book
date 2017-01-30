88967c4635bb05911e88edbbdf458a8f7f9b470a
visvadw <visvadw@gmail.com>
2017-01-30 22:02:29 +0200

    ch 07, page 237

--- webpack/heart-webpack/src/App.js
+++ webpack/heart-webpack/src/App.js
@@ -3,19 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Heart Webpack</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save. Hot reloading does the rest.
         </p>
       </div>
     );
   }
+  
 }
 
 export default App;
